a827c4b7854604e8313696bbead85592c922b9aa
Abdulkadir Musse <72959571+Amusse7@users.noreply.github.com>
2023-12-08 19:41:32 -0500

    quick fix

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
+import Nav from "./(components)/Nav";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Nav from "./(components)/Nav";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
 
