aea9dae65d0f07ee1df8a00b96e8383849552be0
Abdulkadir Musse <72959571+Amusse7@users.noreply.github.com>
2024-07-09 14:51:54 -0400

    few changes

---
+++ app/(components)/DeleteBlock.jsx
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const DeleteBlock = () => {
+  return;
+  <FontAwesomeIcon
+    icon={faX}
+    className="tex-red-400 hover:cursor-pointer hover:text-red-200"
+  />;
+};
+
+export default DeleteBlock;
